eb335bcf9273a57c2d01500e3b773d05dce5d1e8
Anish-Thapa69 <anishthapa696969@gmail.com>
2025-07-18 17:52:24 +0545

    Updated Floor.java

--- Floor.java
+++ Floor.java
@@ -2,35 +2,32 @@ public class Floor {
     
 
     public static void main(String[] args) {
- int[] arr = {2,3,4,9,14,16,18};
-int target = 15;
-System.err.println(binarysearch(arr, target));
-    }
+ char[] letters = {'a', 'c', 'd', 'f', 'g', 's', 's'};
+ char target = 'g';
+ System.out.println(charactersearch(letters, target));
+}
 
-    static int binarysearch(int[] array, int target){
-
-        if(array.length == 0){
-            return -1;
-        }
-
-        int start = 0;
-        int end = array.length -1;
+static int charactersearch(char[] letters, char target){
+    
+  int start = 0;
+        int end = letters.length -1;
 
         while (start <= end) {
             
-            int mid = start + (end - start);
+            int mid = start + (end - start) / 2;
 
-            if(array[mid] == target){
-                return mid;
-            }else if(target > array[mid]){
+           
+             if(target >= letters[mid]){
                 start = mid + 1;
-            }else if(target < array[mid]){
+            }else {
                 end = mid - 1;
            
         }
 
      }
-     return end;
+     
+     return letters[start % letters.length] ;
+
 }
 }
 
